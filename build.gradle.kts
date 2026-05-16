cb1052857eb2fc1ada36f803403aab4ac5b05e40
applnLegal <1029390905@qq.com>
2026-05-17 07:22:18 +0800

    Fix build.gradle.kts - remove allprojects

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,14 +8,3 @@ buildscript {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
     }
 }
-
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-tasks.register("clean", Delete::class) {
-    delete(rootProject.layout.buildDirectory)
-}
